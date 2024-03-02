b8d1f19f5255cd4ed907f2c3a04b9c8c88f98f97
Alikhan <assylkhanov.a1488@gmail.com>
2024-03-02 11:00:00 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+interface PaymentStrategy {
+    void pay(int amount);
+}
+
+class CreditCardPayment implements PaymentStrategy {
+    private String cardNumber;
+
+    public CreditCardPayment(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println("Оплачено " + amount + " с использованием кредитной карты: " + cardNumber);
+    }
+}
+
+class PayPalPayment implements PaymentStrategy {
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println("Оплачено " + amount + " с использованием PayPal: " + email);
+    }
+}
+
+class ShoppingCart {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void checkout(int amount) {
+        if (paymentStrategy != null) {
+            paymentStrategy.pay(amount);
+        } else {
+            System.out.println("Пожалуйста, установите стратегию оплаты перед оформлением заказа.");
+        }
+    }
+}
